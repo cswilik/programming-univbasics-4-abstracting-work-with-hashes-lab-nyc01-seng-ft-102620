66d3bd051b6f7541f049b99fa96564d8b69a0890
Chelsey <cswilik@gmail.com>
2020-10-10 15:24:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,9 @@ def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
   else 
-    hash[key] = hash[key] = 1
+    hash[key] = 1
   end 
+  hash
 end 
   
   # given a hash an a key as parameters, return an updated hash
